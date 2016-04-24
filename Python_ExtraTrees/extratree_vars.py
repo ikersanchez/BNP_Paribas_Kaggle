a4f6dca54ae9d177af3e956a00319536293cf764
ikersanchez <iker.sanchez89@gmail.com>
2016-04-24 13:35:05 +0200

    update

---
+++ Python_ExtraTrees/extratree_vars.py
@@ -0,0 +1,47 @@
+#Extra tree script from kaggle with other variables
+import pandas as pd
+import numpy as np
+import csv
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn import ensemble
+
+
+print('Load data...')
+train = pd.read_csv("train.csv")
+target = train['target'].values
+train = train.drop(['ID','target',"v41", "v95", "v67", "v29", "v20", "v26", "v42", "v53", "v32", "v11", "v68", "v49", "v17", "v94", "v83", "v33", "v118", "v96", "v9", "v65", "v106", "v43", "v78", "v77", "v73", "v19", "v60", "v86", "v100", "v64", "v121", "v13", "v7", "v115", "v25", "v61", "v92", "v48", "v90", "v46", "v70", "v12", "v104", "v37", "v59", "v128", "v55", "v93", "v84", "v63", "v123", "v35", "v5", "v4", "v44", "v111", "v34", "v116", "v36", "v57", "v108", "v87", "v15", "v105", "v27", "v101", "v51", "v6", "v54", "v97", "v45", "v103", "v1", "v99", "v98", "v80", "v18", "v69", "v40", "v76", "v85", "v117", "v126"],axis=1)
+test = pd.read_csv("test.csv")
+id_test = test['ID'].values
+test = test.drop(['ID',"v41", "v95", "v67", "v29", "v20", "v26", "v42", "v53", "v32", "v11", "v68", "v49", "v17", "v94", "v83", "v33", "v118", "v96", "v9", "v65", "v106", "v43", "v78", "v77", "v73", "v19", "v60", "v86", "v100", "v64", "v121", "v13", "v7", "v115", "v25", "v61", "v92", "v48", "v90", "v46", "v70", "v12", "v104", "v37", "v59", "v128", "v55", "v93", "v84", "v63", "v123", "v35", "v5", "v4", "v44", "v111", "v34", "v116", "v36", "v57", "v108", "v87", "v15", "v105", "v27", "v101", "v51", "v6", "v54", "v97", "v45", "v103", "v1", "v99", "v98", "v80", "v18", "v69", "v40", "v76", "v85", "v117", "v126"],axis=1)
+
+print('Clearing...')
+for (train_name, train_series), (test_name, test_series) in zip(train.iteritems(),test.iteritems()):
+    if train_series.dtype == 'O':
+        #for objects: factorize
+        train[train_name], tmp_indexer = pd.factorize(train[train_name])
+        test[test_name] = tmp_indexer.get_indexer(test[test_name])
+        #but now we have -1 values (NaN)
+    else:
+        #for int or float: fill NaN
+        tmp_len = len(train[train_series.isnull()])
+        if tmp_len>0:
+            #print "mean", train_series.mean()
+            train.loc[train_series.isnull(), train_name] = -999 
+        #and Test
+        tmp_len = len(test[test_series.isnull()])
+        if tmp_len>0:
+            test.loc[test_series.isnull(), test_name] = -999
+
+X_train = train
+X_test = test
+print('Training...')
+extc = ExtraTreesClassifier(n_estimators=850,max_features= 30,criterion= 'entropy',min_samples_split= 4,
+                            max_depth= 40, min_samples_leaf= 2, n_jobs = -1)      
+
+extc.fit(X_train,target) 
+
+print('Predict...')
+y_pred = extc.predict_proba(X_test)
+#print y_pred
+pd.DataFrame({"ID": id_test, "PredictedProb": y_pred[:,1]}).to_csv('extra_trees2.csv',index=False)
